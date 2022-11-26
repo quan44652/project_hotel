70cc9e54399c647ead8f4f950d8692bc613c1813
Duy <duytv.dev@gmail.com>
2022-11-26 12:28:33 +0700

    done

--- controller/order/order.php
+++ controller/order/order.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<section class=" layout">
-    <h1>Quản lí hóa đơn</h1>
-    <table border="1">
-        <thead>
-            <tr>
-                <td width="100px">Order ID</td>
-                <td width="180px">User name</td>
-                <td width="180px">Room</td>
-                <td width="200px">Start date</td>
-                <td width="200px">End date</td>
-                <td width="200px">Phone</td>
-                <td width="400px">ACtive</td>
-=======
 <body>
     <div>
         <section class=" layout">
@@ -26,7 +12,6 @@
                         <td width="200px">End date</td>
                         <td width="200px">Phone</td>
                         <td width="300px">ACtive</td>
->>>>>>> 05f5c54f9ca8802ef15e891fb7b84eac522736d2
 
                     </tr>
                 </thead>
@@ -34,19 +19,6 @@
                     <?php
                         foreach ($listbook as $book) {
             ?>
-<<<<<<< HEAD
-                <tr>
-                    <td><?php echo $book['booking_id'] ?></td>
-                    <td><?php echo $book['user_name'] ?></td>
-                    <td><?php echo $book['room_id'] ?></td>
-                    <td><?php echo $book['start_date'] ?></td>
-                    <td><?php echo $book['end_date'] ?></td>
-                    <td><?php echo $book['phone'] ?></td>
-                    <td><a href=""><button class="btn update">CheckIn</button></a> | <a href=""><button class="btn delete">CheckOut</button></a> | <a href="../controller/index.php?act=pay_oder"><button class="btn update pay" style="color: black;">Thanh Toán</button></a></td>
-                </tr>
-                </tr>
-            <?php
-=======
                     <tr>
                         <td>
                             <?php echo $book['booking_id'] ?>
@@ -84,7 +56,6 @@
                     </tr>
                     </tr>
                     <?php
->>>>>>> 05f5c54f9ca8802ef15e891fb7b84eac522736d2
             }
             ?>
                 </tbody>
